cd96949ff99c1876a689ab8f396d1b0f0828ed71
Subhajit Bandyopadhyay <964468+subhajitbn@users.noreply.github.com>
2024-10-31 22:05:32 +0530

    Median absolute deviation
    The phrase "uniquely high level" is now quantified, using MAD.

--- src/desc_stats.py
+++ src/desc_stats.py
@@ -1,4 +1,5 @@
 # Library imports
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -72,11 +73,110 @@ def descriptive_statistics(categories, dfs, biomarker_index):
     plt.show()
     
 
+def identify_outliers_mad(values, threshold_factor=100):
+    """
+    Identify outlier categories in the given list of Q2 values using the
+    Median Absolute Deviation (MAD) approach.
+
+    Parameters
+    ----------
+    Q2_values : list
+        List of Q2 values from the descriptive statistics calculation
+    threshold_factor : int, optional
+        Factor by which the MAD is multiplied to define the threshold for
+        outlier detection (default is 100)
+
+    Returns
+    -------
+    list
+        List of tuples, where each tuple contains the index of the outlier
+        category in the input list and the corresponding Q2 value
+    """
+
+    # Calculate overall median value
+    overall_median = np.median(values)
+
+    # Calculate MAD (Median Absolute Deviation)
+    mad = np.median([abs(x - overall_median) for x in values])
+
+    # Define threshold for outlier detection
+    threshold = overall_median + threshold_factor * mad
+
+    # Identify and return indices of outlier categories
+    outlier_with_indices = [(i,x) for i, x in enumerate(values) if x > threshold]
+    return outlier_with_indices
 
 
+def quantiles_across_categories(categories, dfs, biomarker_index, quantile_cut=0.75):
+    """
+    Calculate the quantiles for the given biomarker across all cancer types.
+
+    Parameters
+    ----------
+    categories : list
+        List of cancer types
+    dfs : list
+        List of DataFrames, each containing the features and labels for a particular cancer type
+    biomarker_index : int
+        Index of the biomarker of interest in the feature DataFrames
+    quantile_cut : float, optional
+        Quantile to calculate (default is 0.75)
+
+    Returns
+    -------
+    list
+        List of quantile values, one for each cancer type
+    """
+    Q_levels = []
+    for df in dfs:
+        features_df = feature_label_split(df)[0]
+        biomarker = features_df.columns[biomarker_index]
+        # Calculate Q2 (median) for the biomarker of interest in the current cancer type
+        Q_value = df[biomarker].quantile(quantile_cut)
+        Q_levels.append(float(Q_value))  # Append the median to the list
+    return Q_levels
+
+def demo_outlier_identification_mad(categories, dfs, biomarker_index):
+    """
+    Demonstrate outlier identification using the Median Absolute Deviation (MAD)
+    approach on Q2 and Q3 values across different cancer types.
+
+    Parameters
+    ----------
+    categories : list
+        List of cancer types
+    dfs : list
+        List of DataFrames, each containing the features and labels for a particular cancer type
+    biomarker_index : int
+        Index of the biomarker of interest in the feature DataFrames
+
+    Returns
+    -------
+    None
+    """
+    biomarker = feature_label_split(dfs[0])[0].columns[biomarker_index]
+    print(f"Biomarker {biomarker_index}: {biomarker}")
+    
+    Q2_levels = quantiles_across_categories(categories, dfs, biomarker_index, quantile_cut=0.5)
+    Q2_levels_with_categories = [(categories[index], Q2_value) for index, Q2_value in enumerate(Q2_levels)]
+    print(f"Q2 values across cancer types: {Q2_levels_with_categories}")
+    Q2_outliers_with_indices = identify_outliers_mad(Q2_levels)
+    Q2_outliers_with_categories = [(categories[index], Q2_value) for index, Q2_value in Q2_outliers_with_indices]
+    print(f"Outlier categories with uniquely high Q2 levels: {Q2_outliers_with_categories}")
+
+    Q3_levels = quantiles_across_categories(categories, dfs, biomarker_index, quantile_cut=0.75)
+    Q3_levels_with_categories = [(categories[index], Q3_value) for index, Q3_value in enumerate(Q3_levels)]
+    print(f"Q3 values across cancer types: {Q3_levels_with_categories}")
+    Q3_outliers_with_indices = identify_outliers_mad(Q3_levels)
+    Q3_outliers_with_categories = [(categories[index], Q3_value) for index, Q3_value in Q3_outliers_with_indices]
+    print(f"Outlier categories with uniquely high Q3 levels: {Q3_outliers_with_categories}")
 
 # Debug code
 if __name__ == "__main__":
     categories, dfs = load_data()
-    biomarker_index = 0
-    descriptive_statistics(categories, dfs, biomarker_index)
+    # Biomarkers: AFP: 0, CA-125: 3, CA19-9: 5, Prolactin: 29, sHER2/sEGFR2/sErbB2: 33
+
+    # descriptive_statistics(categories, dfs, biomarker_index = 0)
+    for i in [0, 3, 5, 29, 33]:
+        demo_outlier_identification_mad(categories, dfs, biomarker_index = i)
+        print("\n")
